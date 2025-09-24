027967a477460a6c299e07fabe35d65a4c69a7ed
franzooor <hagengu@web.de>
2025-09-24 11:08:33 +0200

    Switch auto-combat toggle to G key

--- isaac_auto_combat/config/defaults.lua
+++ isaac_auto_combat/config/defaults.lua
@@ -1,6 +1,12 @@
 -- Default configuration values for the auto combat mod.
+local toggleKey = nil
+if Keyboard ~= nil then
+  toggleKey = Keyboard.KEY_G
+end
+
 return {
-  toggleAction = ButtonAction.ACTION_MAP,
+  toggleAction = nil,
+  toggleKey = toggleKey,
   overlay = {
     enabled = true,
     page = 1,
